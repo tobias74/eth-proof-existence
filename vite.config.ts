b3cacd5a8386d96a096d55126050d149b1bc1262
Tobias Gassmann <tobias@gaszmann.de>
2024-10-02 14:10:54 +0200

    fix the buffer externalized error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      // Polyfill `global` and other necessary Node.js globals
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      // Polyfill for `buffer` and other Node.js core modules
+      buffer: 'buffer',
+      util: 'util',
+      process: 'process/browser',
+      stream: 'stream-browserify',
+      http: 'stream-http',
+      crypto: 'crypto-browserify',
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+});
