3b8b7e17ede6353e5a3af2f5d5b6c470f87a5a59
Tobias Gassmann <tobias@gaszmann.de>
2024-10-04 17:58:03 +0200

    fix 6

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    esbuildOptions: {
-      // Polyfill `global` and other necessary Node.js globals
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-      ],
-    },
   },
   resolve: {
-    alias: {
-    },
   },
   build: {
-    rollupOptions: {
-      plugins: [
-      ],
-    },
   },
 });
