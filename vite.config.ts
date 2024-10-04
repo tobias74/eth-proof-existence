ee270494c84a4c328a46dfccae117be105f4b398
Tobias Gassmann <tobias@gaszmann.de>
2024-10-04 18:22:05 +0200

    fix 9

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-  },
-  resolve: {
-  },
-  build: {
-  },
+  assetsInclude: ['**/locales/**']  // Ensure locales folder is included
 });
